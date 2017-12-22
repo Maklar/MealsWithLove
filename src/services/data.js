2714a01805e7309bb317763fe0eb1e8c62f40a59
Jonathan Moffitt <moffitts1@yahoo.com>
2017-12-22 17:44:07 -0500

    Updates data service to use process environment

--- src/services/data.js
+++ src/services/data.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default class Data  {
   constructor() {
-    axios.defaults.baseURL = 'http://localhost:3001';    
+    axios.defaults.baseURL = process.env.REACT_APP_DATA_URL || 'http://localhost:3002';    
     axios.defaults.headers.post['Content-Type'] = 'application/json';
   }
 
