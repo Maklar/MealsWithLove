a735511b3ec22145b5c30e19ba7f356eca23ded5
Jonathan Moffitt <moffitts1@yahoo.com>
2017-12-23 12:48:56 -0500

    Configures data service to use oauth authentication

--- src/services/data.js
+++ src/services/data.js
@@ -2,8 +2,9 @@ import axios from "axios";
 
 export default class Data  {
   constructor() {
-    axios.defaults.baseURL = process.env.REACT_APP_DATA_URL || 'http://localhost:3002';    
+    axios.defaults.baseURL = process.env.REACT_APP_DATA_URL || 'http://localhost:3001';    
     axios.defaults.headers.post['Content-Type'] = 'application/json';
+    axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('id_token') || '';
   }
 
   getUserByOAuthId(oauthId, cb) {
